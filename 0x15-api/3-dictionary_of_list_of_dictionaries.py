e79dae1a9cef827fbe9ddf5816b6f15501098fd3
logmar98 <marwanarbib15@gmail.com>
2024-02-19 19:10:54 +0100

    update task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,7 +5,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    for arg in range(10):
+    obj = {}
+    for arg in range(1, 11):
         url = "https://jsonplaceholder.typicode.com/users/{}".format(arg)
         resp_todo = requests.get('{}/todos'.format(url))
         resp_user = requests.get('{}'.format(url))
@@ -16,15 +17,15 @@ if __name__ == "__main__":
         tasks = len(todo_data)
         user_name = user_data['username']
 
-        obj = {'{}'.format(arg): []}
+        obj['{}'.format(arg)] = []
 
         for item in todo_data:
             todo_obj = {}
+            todo_obj['username'] = user_name
             todo_obj['task'] = item["title"]
             todo_obj['completed'] = item['completed']
-            todo_obj['username'] = user_name
             obj['{}'.format(arg)].append(todo_obj)
 
-        json_data = json.dumps(obj)
-        with open('all.json', 'w') as file:
-            file.write(json_data)
+    json_data = json.dumps(obj)
+    with open('todo_all_employees.json', 'w') as file:
+        file.write(json_data)
